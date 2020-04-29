4aacba8fc166da03e073a15c52e773ca07534eb4
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-04-29 02:14:39 +0100

    Generate trophies module along with routing

---
+++ src/app/trophies/trophies.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TrophiesRoutingModule } from './trophies-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, TrophiesRoutingModule],
+})
+export class TrophiesModule {}
