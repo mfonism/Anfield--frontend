842d6512597e024b06081e9dcb8030981c71cc17
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-04-29 01:12:45 +0100

    Workon home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 import { finalize } from 'rxjs/operators';
 
 import { QuoteService } from './quote.service';
@@ -24,7 +25,12 @@ export class HomeComponent implements OnInit {
         })
       )
       .subscribe((quote: string) => {
-        this.quote = quote;
+        const VIPs = ['Sadio Mané', 'Jürgen Klopp', 'Mohamed Salah'];
+        var vip = _.sample(VIPs);
+        var vip_split = vip.split(' ');
+        this.quote = quote.replace('Chuck Norris', vip);
+        this.quote = this.quote.replace('Chuck', vip_split[0]);
+        this.quote = this.quote.replace('Norris', vip_split[1]);
       });
   }
 }
