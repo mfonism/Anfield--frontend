2eeb6bba203666c73b1173993f37539d52d5d095
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-05-05 23:30:48 +0100

    Enable email + password sign-in with firebase
    * First add Firebase to the (frontend) project
      https://firebase.google.com/docs/web/setup
    * Then enable email + password sign-in with Firebase
      https://firebase.google.com/docs/auth/web/password-auth

---
+++ src/app/firebase-setup/index.ts
@@ -0,0 +1,19 @@
+var firebase = require('firebase/app');
+
+require('firebase/auth');
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDiW7iRvKQ-Eg5s1LKqo5EgCE7vroL-CNw',
+  authDomain: 'ynwa-e214d.firebaseapp.com',
+  databaseURL: 'https://ynwa-e214d.firebaseio.com',
+  projectId: 'ynwa-e214d',
+  storageBucket: 'ynwa-e214d.appspot.com',
+  messagingSenderId: '849378998821',
+  appId: '1:849378998821:web:c56f70ea338bb15fd1ddcc',
+  measurementId: 'G-FLSN2XCLGM',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
