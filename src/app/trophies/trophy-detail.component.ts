a9115294dfc47a00d71e858ca53c371fd96584ad
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-06-05 01:28:19 +0100

    Fall back to 404 on error reading trophy detail

--- src/app/trophies/trophy-detail.component.ts
+++ src/app/trophies/trophy-detail.component.ts
@@ -14,14 +14,23 @@ export class TrophyDetailComponent implements OnInit {
   selectedId: string;
   trophy: ITrophy;
 
-  constructor(private activatedRoute: ActivatedRoute, private trophiesService: TrophiesService) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private trophiesService: TrophiesService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
       this.selectedId = params.get('id');
     });
-    this.trophiesService.getTrophy(this.selectedId).subscribe((trophy: ITrophy) => {
-      this.trophy = trophy;
-    });
+    this.trophiesService.getTrophy(this.selectedId).subscribe(
+      (trophy: ITrophy) => {
+        this.trophy = trophy;
+      },
+      (error: any) => {
+        this.router.navigate(['/not-found']);
+      }
+    );
   }
 }
